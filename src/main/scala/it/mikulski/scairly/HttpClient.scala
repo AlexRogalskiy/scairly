abf53065a6a64dc1612a47296df00c43264bad54
jakub.mikulski <kuba.mikulski@gmail.com>
2018-12-02 00:35:22 +0100

    gzip support

--- src/main/scala/it/mikulski/scairly/HttpClient.scala
+++ src/main/scala/it/mikulski/scairly/HttpClient.scala
@@ -2,7 +2,7 @@ package it.mikulski.scairly
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.coding.Gzip
+import akka.http.scaladsl.coding.{ Gzip, NoCoding }
 import akka.http.scaladsl.model.headers.{ HttpEncodings, RawHeader }
 import akka.http.scaladsl.model.{ HttpMethods, HttpRequest, HttpResponse, Uri }
 import akka.http.scaladsl.unmarshalling.Unmarshaller
@@ -23,20 +23,25 @@ class HttpClient(apiKey: String)(implicit val system: ActorSystem) {
     val request = HttpRequest(HttpMethods.GET, uri = Uri(s"$baseUrl$path?$query"))
       .addHeader(RawHeader("apikey", apiKey))
       .addHeader(RawHeader("Accept", "application/json"))
-    //.addHeader(RawHeader("Accept-Encoding", "gzip"))
+      .addHeader(RawHeader("Accept-Encoding", "gzip"))
     for {
       response <- http.singleRequest(request)
-      parsed <- unmarshaller.apply(response)
+      parsed <- unmarshaller.apply(decode(response))
     } yield parsed
   }
 
-  private def responseBodyAsString(response: HttpResponse)(implicit mat: ActorMaterializer, ec: ExecutionContext): Future[String] =
+  private def decode(response: HttpResponse): HttpResponse = {
+    val decoder = response.encoding match {
+      case HttpEncodings.gzip ⇒ Gzip
+      case _ ⇒ NoCoding
+    }
+    decoder.decodeMessage(response)
+  }
+
+  private def responseBodyAsString(response: HttpResponse)(implicit mat: ActorMaterializer, ec: ExecutionContext): Future[String] = {
     for {
-      bs <- response.entity.dataBytes.runFold(ByteString(""))(_ ++ _)
-      ds <- response.encoding match {
-        case HttpEncodings.gzip => Gzip.decode(bs)
-        case _ => Future.successful(bs)
-      }
-    } yield ds.utf8String
+      bs <- decode(response).entity.dataBytes.runFold(ByteString(""))(_ ++ _)
+    } yield bs.utf8String
+  }
 
 }
